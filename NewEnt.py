c71bc6c240859c4db12094794767866e3c006c07
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-02-16 17:07:45 -0600

    Update NewEnt.py
    UPDATED to require stars.py module

--- NewEnt.py
+++ NewEnt.py
@@ -2,32 +2,55 @@
 from microbit import *
 import random
 import time
-
-def fnstars():
-    for i in range(5):
-        if random.randrange(10)>8:
-            display.set_pixel(i,0,1+random.randrange(7))
-        else:
-            display.set_pixel(i,0,0)
+import music
+import speech
+from stars import *
 
 
-def fnslide():
-    for i in range(4):
-        for j in range(5):
-            display.set_pixel(j,4-i,display.get_pixel(j,3-i))
+
 
 
 def blast(x):
     for i in range(5):
+        if display.get_pixel(x,4-i) == 8:
+            #Klingon!
+            score = score + 1+ random.randrange(10)
+            speech.say("Klingon!")
+            music.BLUES
         display.set_pixel(x,4-i,9)
 
 shipx = 2
 shipy = 4
+score = 0
+for x in range(30):
+    fnstarsS()
+    fnslideS()
+    time.sleep(.1)
+
+for x in range(30):
+    fnstarsS()
+    fnslideS()
+    time.sleep(.1)
+
+
+for x in range(30):
+    fnstarsE()
+    fnslideE()
+    time.sleep(.1)
+
+for x in range(30):
+    fnstarsW()
+    fnslideW()
+    time.sleep(.1)
+
 while True:
     fnstars()
     fnslide()
     display.set_pixel(shipx,shipy,9)
     time.sleep(.2)
+    
+    if accelerometer.was_gesture('shake'):
+       blast(shipx)
     
     if button_a.was_pressed() and button_b.was_pressed():
         blast(shipx)
