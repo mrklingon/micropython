497e295a11f12993b9a6b12f3b792ff6eba39d01
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-02-16 19:19:28 -0600

    Add files via upload
    variant using stars.py to rotate star field. A/B switches direction A+B pauses

---
+++ NewEnt2-main.py
@@ -0,0 +1,115 @@
+# Imports go at the top
+from microbit import *
+import random
+import time
+import music
+import speech
+from stars import *
+
+
+
+
+
+def blast(x):
+    for i in range(5):
+        if display.get_pixel(x,4-i) == 8:
+            #Klingon!
+            score = score + 1+ random.randrange(10)
+            speech.say("Klingon!")
+            music.BLUES
+        display.set_pixel(x,4-i,9)
+
+shipx = 2
+shipy = 4
+score = 0
+for x in range(30):
+    fnstarsS()
+    fnslideS()
+    time.sleep(.1)
+
+for x in range(30):
+    fnstarsS()
+    fnslideS()
+    time.sleep(.1)
+
+
+for x in range(30):
+    fnstarsE()
+    fnslideE()
+    time.sleep(.1)
+
+for x in range(30):
+    fnstarsW()
+    fnslideW()
+    time.sleep(.1)
+
+dirs = [0,1,2,3]
+north = 0
+east = 1
+south = 2
+west = 3
+
+dir = north
+motion = True
+
+
+while True:
+    if motion:
+        if dir == north:
+            shipx = 2
+            shipy = 4
+            fnstars()
+            fnslide()
+            display.set_pixel(shipx,shipy,9)
+            time.sleep(.2)
+    
+        
+        if dir == east:
+            shipy = 2
+            shipx = 4
+            fnstarsE()
+            fnslideE()
+            display.set_pixel(shipx,shipy,9)
+            time.sleep(.2)
+    
+        
+        if dir == south:
+            shipy = 0
+            shipx = 2
+            fnstarsS()
+            fnslideS()
+            display.set_pixel(shipx,shipy,9)
+            time.sleep(.2)
+    
+           
+        if dir == west:
+            shipy = 2
+            shipx = 0
+            fnstarsW()
+            fnslideW()
+            display.set_pixel(shipx,shipy,9)
+            time.sleep(.2)
+     
+   
+    if button_a.was_pressed() and button_b.was_pressed():
+        if motion:
+            motion = False
+        else:
+            motion = True
+        
+
+    
+    if button_a.is_pressed():
+       motion = True
+       dir = dir+1
+       if dir > 3:
+           dir = 0
+    
+    if button_b.is_pressed():
+       motion = True
+       dir = dir-1
+       if dir < 0:
+           dir = 3
+       
+       
+    
