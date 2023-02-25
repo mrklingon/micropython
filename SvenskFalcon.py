ce211896e850e502a9861e20d905983e8d008951
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-02-25 15:24:27 -0600

    Update SvenskFalcon.py
    reverse direction added,sound with torpedo.

--- SvenskFalcon.py
+++ SvenskFalcon.py
@@ -48,6 +48,36 @@ while True:
                shipy = shipy + 1
                if shipy > 4:
                    shipy=4
+    lives = 3
+    dir = east
+    shipx = 4 
+    shipy = 2
+    display.clear()
+    while lives > 0:
+        score = score + 1    
+        fnstarsE()
+        fnslideE()
+        if display.get_pixel(shipx,shipy) != 0:
+            music.play(music.BA_DING)
+            lives = lives - 1
+        display.set_pixel(shipx,shipy,9)
+        time.sleep(.2)
+        
+        if accelerometer.was_gesture('shake') or pin1.is_touched():
+            blast(shipx,shipy,dir)
+    
+        if button_a.is_pressed() and button_b.is_pressed():
+            blast(shipx,shipy,dir)
+        else:
+            if button_a.is_pressed():
+              shipy = shipy - 1
+              if shipy < 0:
+                  shipy = 0
+            
+            if button_b.is_pressed():
+               shipy = shipy + 1
+               if shipy > 4:
+                   shipy=4
                
     display.show(Image.SKULL)
     music.play(music.FUNERAL)
