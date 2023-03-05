88f81c771f41e4e3e2d1521b565ab121e960f32b
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-03-05 16:31:47 -0600

    Update StarTravel.py

--- StarTravel.py
+++ StarTravel.py
@@ -5,13 +5,54 @@ import time
 diam = 30
 initUni(diam)
 
-for x in range(diam-5):
+for x in range(5):
     showxy(x,x)
     time.sleep(.33)
 
-# Code in a 'while True:' loop repeats forever
+north = 0
+east = 1
+south = 2
+west = 3
+stop = 5
+
+x=0
+y=0
+
+dirs = [north,east,south,west]
+
+dir = north
+
 while True:
-   for y in range(diam-5):
-       for x in range(diam-5):
-           showxy(x,y)
-           time.sleep(.25)
+    if button_a.was_pressed() and button_b.was_pressed():
+        dir = stop
+    if accelerometer.was_gesture('up'):
+        dir=north
+    if accelerometer.was_gesture('down'):
+        dir=south
+    if accelerometer.was_gesture('right'):
+        dir=west
+    if accelerometer.was_gesture('left'):
+        dir=east
+    
+    if dir == west:
+        x=x-1
+        if x<0:
+            x=diam-5
+    
+    if dir == east:
+        x=x+1
+        if x>diam+1:
+            x=0
+   
+    if dir == north:
+        y=y-1
+        if y<0:
+            y=diam-5
+    if dir == south:
+        y=y+1
+        if y>diam-5:
+            y = 0     
+
+
+    showxy(x,y)
+    time.sleep(.25)
