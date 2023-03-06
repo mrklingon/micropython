afb388e5afc5efa36aad2544a830bf5ea490e35c
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-03-06 16:48:11 -0600

    Update universe.py

--- universe.py
+++ universe.py
@@ -3,18 +3,22 @@ from microbit import *
 import random
 
 cosmos = []
+names = []
 
 def initUni(diam):
     global cosmos
     global dia
+    global names
 
     dia = diam
     
     for x in range (diam * diam):
         if random.randrange(100)>60:
             cosmos.append(random.randrange(5))
+            names.append(placeNM())
         else:
             cosmos.append(0)
+            names.append("")
 
 def showxy(x,y):
     global cosmos
@@ -23,7 +27,19 @@ def showxy(x,y):
     display.clear()
     for dy in range(5):
         for dx in range(5):
-            cell = x+dx+(y+dy)*dia
+            cell = x+dx+((y+dy)*dia)
             if cosmos[cell]>0:
                 display.set_pixel(dx,dy,5+cosmos[cell])
+def spot(x,y):
+    cell = x+(y*dia)
+    return (cell)
 
+    
+def placeNM():
+    
+    start = ["Gi","Ro","Ah","Mi","Pa","Ki","Re","Sy","Th","Zo"]
+    mid = ["eu","af","gh","uu","a","e","i","o","u","y"]
+    end = ["nk","as","of","z","d","n","ll","ah","ei","ie"]
+
+    Place = random.choice(start)+random.choice(mid)+random.choice(end)
+    return (Place)
